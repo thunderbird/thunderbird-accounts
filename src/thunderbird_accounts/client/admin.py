c9e0b91f2f77833252d66ab1c25108eefbe8d635
Melissa Autumn <melissa@thunderbird.net>
2024-11-12 13:23:38 -0800

    Add migrations and some additional admin views

--- src/thunderbird_accounts/client/admin.py
+++ src/thunderbird_accounts/client/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from thunderbird_accounts.client.models import Client
+from thunderbird_accounts.client.models import Client, ClientContact, ClientEnvironment, ClientWebhook
 
-admin.site.register(Client)
+
+class ClientContactInline(admin.TabularInline):
+    model = ClientContact
+    extra = 1
+
+
+class ClientEnvironmentInline(admin.TabularInline):
+    model = ClientEnvironment
+    extra = 1
+    fields = ('environment', 'redirect_url')
+
+
+class ClientAdmin(admin.ModelAdmin):
+    inlines = (
+        ClientContactInline,
+        ClientEnvironmentInline,
+    )
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(ClientContact)
+admin.site.register(ClientEnvironment)
+admin.site.register(ClientWebhook)
--- src/thunderbird_accounts/subscription/admin.py
+++ src/thunderbird_accounts/subscription/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from thunderbird_accounts.subscription.models import Subscription
+from thunderbird_accounts.subscription.models import Subscription, Customer
 
+admin.site.register(Customer)
 admin.site.register(Subscription)
