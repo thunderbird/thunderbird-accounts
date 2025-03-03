46677e697354979c2e7ebc718083139dd55b66ec
rwood-moz <rwood@thunderbird.net>
2025-03-03 15:52:25 -0500

    Add E2E test for sign-in/out to self-serve hub

---
+++ test/e2e/const/constants.ts
@@ -0,0 +1,12 @@
+// environment where the tests will run
+export const ACCTS_TARGET_ENV = String(process.env.ACCTS_TARGET_ENV);
+
+// tb accounts urls
+export const ACCTS_SELF_SERVE_URL = String(process.env.ACCTS_SELF_SERVE_URL);
+
+// sign-in credentials and corresponding account display name
+export const ACCTS_FXA_EMAIL = String(process.env.ACCTS_FXA_EMAIL);
+export const ACCTS_FXA_PWORD = String(process.env.ACCTS_FXA_PWORD);
+
+// playwright test tags
+export const PLAYWRIGHT_TAG_E2E_SUITE = '@e2e-suite';
