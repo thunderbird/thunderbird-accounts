b30c3e987f33c7f7a50150de9346fd26475ae75a
Chris Aquino <chris@thunderbird.net>
2025-04-15 18:23:33 -0400

    Add CheckoutFlow vue component

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,9 +1,10 @@
-import {createApp} from "vue";
+import { createApp } from 'vue';
 import '@thunderbirdops/services-ui/style.css';
-import SelfServeForm from "@/components/SelfServeForm.vue";
-import SignInToSignUpForm from "@/components/SignInToSignUpForm.vue";
-import SignUpFlowForm from "@/components/SignUpFlowForm.vue";
-import AccountInfoForm from "@/components/AccountInfoForm.vue";
+import SelfServeForm from '@/components/SelfServeForm.vue';
+import SignInToSignUpForm from '@/components/SignInToSignUpForm.vue';
+import SignUpFlowForm from '@/components/SignUpFlowForm.vue';
+import AccountInfoForm from '@/components/AccountInfoForm.vue';
+import CheckoutFlow from '@/components/CheckoutFlow.vue';
 
 const vueApps = [
   // Self Serve - App Password Page
@@ -23,8 +24,12 @@ const vueApps = [
   },
   {
     id: 'signUpFlowForm',
-    sfc: SignUpFlowForm
-  }
+    sfc: SignUpFlowForm,
+  },
+  {
+    id: 'checkoutFlow',
+    sfc: CheckoutFlow,
+  },
 ];
 
 for (const app of vueApps) {
---
+++ assets/app/vue/components/CheckoutFlow.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { initializePaddle } from '@paddle/paddle-js';
+
+const isLoading = ref(true);
+const priceItems = ref([]);
+
+const paddleToken = window._page.paddleToken;
+const paddleEnvironment = window._page.paddleEnvironment;
+const paddlePriceIdLo = window._page.paddlePriceIdLo;
+const paddlePriceIdMd = window._page.paddlePriceIdMd;
+const paddlePriceIdHi = window._page.paddlePriceIdHi;
+const successRedirect = window._page.successRedirect;
+
+const baseUrl = window.location
+  .toString()
+  .replace(window.location.pathname, '');
+const successUrl = `${baseUrl}${successRedirect}`;
+
+let Paddle; // The initialized Paddle instance.
+
+const paddleItems = [paddlePriceIdLo, paddlePriceIdMd, paddlePriceIdHi].map(
+  (priceId) => ({
+    quantity: 1,
+    priceId,
+  })
+);
+
+console.log(`TODO: put environment value in actual env var`);
+
+onMounted(() => {
+  initializePaddle({
+    environment: paddleEnvironment,
+    token: paddleToken,
+  }).then((paddleInstance) => {
+    if (!paddleInstance) {
+      return;
+    }
+    Paddle = paddleInstance;
+    initPaddle(paddleItems);
+  });
+});
+
+function openCheckout(priceId) {
+  Paddle.Checkout.open({
+    settings: {
+      successUrl,
+    },
+    items: [
+      {
+        quantity: 1,
+        priceId,
+      },
+    ],
+  });
+}
+
+function initPaddle(items, fn) {
+  Paddle.PricePreview({ items })
+    .then((result) => {
+      const { lineItems } = result.data.details;
+      priceItems.value = lineItems.map((item) => {
+        const { total } = item.formattedTotals;
+        const { name, description, id } = item.price;
+        return {
+          total,
+          name,
+          description,
+          id,
+        };
+      });
+      isLoading.value = false;
+    })
+    .then(fn)
+    .catch((error) => {
+      console.error(error);
+    });
+}
+</script>
+<template>
+  <div class="pricing-page-container">
+    <h1>Choose your plan</h1>
+    <div v-if="isLoading" class="loader-outside">
+      <div class="loader-inside"></div>
+    </div>
+    <div v-else class="pricing-grid" id="pricing-grid">
+      <div v-for="item in priceItems">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+        <p>{{ item.total }}</p>
+        <button @click="openCheckout(item.id)">Select {{ item.total }}</button>
+      </div>
+    </div>
+  </div>
+</template>
