b19d081a7acdae6bede656d981e27b80a4b11b80
Melissa Autumn <melissa@thunderbird.net>
2025-05-21 14:47:19 -0700

    Expose Transaction models to the admin panel

--- src/thunderbird_accounts/subscription/admin.py
+++ src/thunderbird_accounts/subscription/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from thunderbird_accounts.subscription.models import Subscription, Plan, SubscriptionItem, Price
+from thunderbird_accounts.subscription.models import Subscription, Plan, SubscriptionItem, Price, Transaction
 
 admin.site.register(Subscription)
 admin.site.register(SubscriptionItem)
+admin.site.register(Transaction)
 admin.site.register(Plan)
 admin.site.register(Price)
