6aa29bf830645e80c10659b66b98b277f4da1cd4
Ryan Jung <rjung@thunderbird.net>
2025-05-28 13:17:20 -0600

    Add input validation with better messaging

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -10,6 +10,11 @@ import tb_pulumi.fargate
 import tb_pulumi.network
 import tb_pulumi.secrets
 
+
+MSG_LB_MATCHING_CONTAINER='In this stack, container security groups must have matching load balancer security groups.'
+MSG_LB_MATCHING_CLUSTER='In this stack, Fargate clusters must have matching load balancer security groups.'
+MSG_CONTAINER_MATCHING_CLUSTER='In this stack, Fargate clusters must have matching container security groups.'
+
 # Set up the project and convenient config access
 project = tb_pulumi.ThunderbirdPulumiProject()
 resources = project.config.get('resources')
@@ -41,6 +46,8 @@ lb_sgs = {
 # Build security groups for containers
 container_sgs = {}
 for service, sg in resources['tb:network:SecurityGroupWithRules']['containers'].items():
+    if service not in lb_sgs:
+        raise ValueError(f'{MSG_LB_MATCHING_CONTAINER} Create a matching load_balancers entry for "{service}".')
     # Allow access from each load balancer to its respective container
     for rule in sg['rules']['ingress']:
         rule['source_security_group_id'] = lb_sgs[service].resources['sg'].id
@@ -79,6 +86,10 @@ redis = tb_pulumi.elasticache.ElastiCacheReplicationGroup(
 # Build Fargate clusters to run our containers
 fargate_clusters = {}
 for service, opts in resources['tb:fargate:FargateClusterWithLogging'].items():
+    if service not in lb_sgs:
+        raise ValueError(f'{MSG_LB_MATCHING_CLUSTER} Create a matching load_balancers entry for "{service}".')
+    if service not in container_sgs:
+        raise ValueError(f'{MSG_CONTAINER_MATCHING_CLUSTER} Create a matching load_balancers entry for "{service}".')
     lb_sg_ids = [lb_sgs[service].resources['sg'].id] if lb_sgs[service] else []
     depends_on = [
         container_sgs[service].resources['sg'],
