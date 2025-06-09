fda5e871070d2cb38097dfc020806a2243957dc0
Ryan Jung <rjung@thunderbird.net>
2025-06-09 14:01:39 -0600

    Consolidate container build/runtime files

--- scripts/celery-entry.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-uv run celery -A thunderbird_accounts worker -l INFO
--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
+# Usage:
+#   Running this script with the default options will launch the backend in an environment-ready
+#   mode. To run in development mode instead, run with TBA_DEV=yes. To run the Celery worker
+#   instead, run with TBA_CELERY=yes.
 
 # Build the vite apps
 npm run build
 
 # Collect static content, ignore the vue component directory
 ./manage.py collectstatic --noinput -i assets/app/vue
+
+# In dev environments, give the DB a little extra time to come online
+if [[ "$TBA_DEV" == "yes" ]]; then
+    echo 'Waiting 1s for DB...'
+    sleep 1s
+fi
 
 # Run migrations
 ./manage.py migrate
@@ -14,8 +24,15 @@ npm run build
 ./manage.py get_paddle_products
 ./manage.py get_paddle_prices
 
-# Classic Django devserver
-#./manage.py runserver 0.0.0.0:8087
+# Run the app with the appropriate command
+if [[ "$TBA_CELERY" == "yes" ]]; then
+    CMD="uv run celery -A thunderbird_accounts worker -l INFO"
+else
+    CMD="uv run uvicorn thunderbird_accounts.asgi:application $ARGS"
+    ARGS="--lifespan off --host 0.0.0.0 --port 8087"
+    if [[ "$TBA_DEV" == "yes" ]]; then
+        ARGS="$ARGS --reload --reload-include *.html"
+    fi
+fi
 
-# Uvicorn devserver (preferred)
-uv run uvicorn thunderbird_accounts.asgi:application --lifespan off --host 0.0.0.0 --port 8087
+$CMD $ARGS
