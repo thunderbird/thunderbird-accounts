3a7cb9a5a72cc47924af6c1ac0c6ecc66329b739
Melissa Autumn <melissa@thunderbird.net>
2025-06-27 14:45:25 -0700

    Update keycloak example theme to pre-fill timezone via js

--- keycloak/themes/tbpro/login/resources/js/script.js
+++ keycloak/themes/tbpro/login/resources/js/script.js
@@ -1,1 +1,7 @@
-alert('Hello');
+window.addEventListener('load', (evt) => {
+  const zoneinfo = document.getElementById('zoneinfo');
+  if (zoneinfo) {
+    zoneinfo.value = Intl.DateTimeFormat().resolvedOptions().timeZone ?? 'UTC';
+    zoneinfo.setAttribute('readonly', 'readonly');
+  }
+});
