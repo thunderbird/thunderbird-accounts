9b71255563d404f1f2a9f22233d5e7e015ec9c58
Melissa Autumn <melissa@thunderbird.net>
2025-07-08 13:44:00 -0700

    Simplify bootstrap by using basic auth for dev environment

--- bootstrap.py
+++ bootstrap.py
@@ -17,14 +17,11 @@ args = arg_parse.parse_args()
 def main():
     from_scratch = args.from_scratch
 
-    new_env = False
-
     print('Bootstrapping project:')
     if not os.path.isfile('.env') or from_scratch:
         try:
             shutil.copy('.env.example', '.env')
             print('\t* Copied .env.example to .env')
-            new_env = True
         except SameFileError:
             pass
 
@@ -38,16 +35,6 @@ def main():
             print('\t* Copied config.toml.example to mail/etc/config.toml')
         except SameFileError:
             pass
-
-    print('\n')
-    if new_env:
-        print('Please boot up docker-compose.yml in another window...')
-        input('Press any key when docker is booted!')
-
-        print('Creating an api key')
-        os.system('./manage.py create_stalwart_api_key')
-
-        print('Please restart docker before developing')
 
     print('Finished!')
 
