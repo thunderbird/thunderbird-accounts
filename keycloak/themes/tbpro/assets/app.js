a197ddf80f10bffe43f6c4c0b5644326276917dd
Melissa Autumn <melissa@thunderbird.net>
2025-07-29 14:46:24 -0700

    Add annoying work-around for browserhistoryhelper breaking routes

--- keycloak/themes/tbpro/assets/app.js
+++ keycloak/themes/tbpro/assets/app.js
@@ -22,9 +22,21 @@ const path = window.location.pathname.replace(import.meta.env.VITE_BASE_PATH, '/
 router.push(path)
 console.log('current route: ', router.currentRoute?.value)
 
+// Small hack to get around BrowserHistoryHelper
+// https://github.com/keycloak/keycloak/blob/75ade9acef443ca98764443eb61f443b701b87dd/services/src/main/java/org/keycloak/services/resources/LoginActionsService.java#L390C51-L390C74
+window.history._replaceState = window.history.replaceState;
+window.history.replaceState = (state, title, url) => {
+  // Call the real replaceState
+  window.history._replaceState(state, title, url);
+  // Update our router
+  const path = url.replace(window.location.origin, '').replace(import.meta.env.VITE_BASE_PATH, '/');
+  router.push(path);
+}
+
 // This isn't a module, so wait until the document is loaded before mounting
 window.addEventListener('load', () => {
   // Load our l10n messages once the template is fully rendered.
   i18n.global.mergeLocaleMessage('en', {...window?._l10n ?? {}})
   app.mount('#app');
 });
+
