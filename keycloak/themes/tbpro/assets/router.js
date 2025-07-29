e95534d6b61737b0ab486c210685e18d9548272a
Melissa Autumn <melissa@thunderbird.net>
2025-07-29 15:19:04 -0700

    Add a page expired route that activates on pageId.

--- keycloak/themes/tbpro/assets/router.js
+++ keycloak/themes/tbpro/assets/router.js
@@ -2,6 +2,7 @@ import { createMemoryHistory, createRouter } from "vue-router";
 import LoginView from "@/vue/views/LoginView/index.vue";
 import RegisterView from "@/vue/views/RegisterView/index.vue";
 import ForgotPassword from "@/vue/views/ForgotPassword/index.vue";
+import PageExpiredView from "./vue/views/PageExpiredView/index.vue";
 
 
 const routes = [
@@ -23,6 +24,11 @@ const routes = [
     path: '/login-actions/reset-credentials',
     name: 'forgot-password',
     component: ForgotPassword,
+  },
+  {
+    path: '/login-actions/reset-credentials',
+    name: 'page-expired',
+    component: PageExpiredView,
   }
 ];
 
--- keycloak/themes/tbpro/assets/vue/App.vue
+++ keycloak/themes/tbpro/assets/vue/App.vue
@@ -1,9 +1,23 @@
-<script setup lang="ts">
+<script setup>
+import { computed, ref } from "vue";
+import router from "../router";
+
   console.log("And attached!!");
+
+  console.log('page id', window._page.pageId);
+
+  // Work-around for pages that don't have unique routes
+  const pageId = ref(window._page.pageId);
+  if (pageId.value === 'login-page-expired') {
+    router.replace({ name: 'page-expired' });
+  }
+
+
 </script>
 
 <template>
   <section class="centre-panel">
+    {{pageId}}
     <router-view/>
   </section>
 
