1eadcd99b9af1fbac3c64c88b8f131e5f3cbe3b0
Davi Nakano <davinakanoca@gmail.com>
2025-09-15 11:21:30 -0600

    add ua-parser and regex for user-agent parsing

--- src/thunderbird_accounts/utils/utils.py
+++ src/thunderbird_accounts/utils/utils.py
@@ -1,5 +1,6 @@
 import json
 from urllib.parse import urljoin
+from ua_parser import parse_user_agent, parse_os
 
 from django.conf import settings
 
@@ -25,3 +26,29 @@ class JsonSerializer:
 
     def loads(self, obj):
         return json.loads(obj)
+
+
+def parse_user_agent_info(user_agent_string: str) -> tuple[str, str]:
+    """Parse user agent string and return separate browser and OS information."""
+
+    # Extract browser information
+    browser_info = parse_user_agent(user_agent_string)
+    browser_string = f"{browser_info.family}"
+    if browser_info.major:
+        browser_string += f" {browser_info.major}"
+        if browser_info.minor:
+            browser_string += f".{browser_info.minor}"
+            if browser_info.patch:
+                browser_string += f".{browser_info.patch}"
+
+    # Extract OS information
+    os_info = parse_os(user_agent_string)
+    os_string = f"{os_info.family}"
+    if os_info.major:
+        os_string += f" {os_info.major}"
+        if os_info.minor:
+            os_string += f".{os_info.minor}"
+            if os_info.patch:
+                os_string += f".{os_info.patch}"
+
+    return browser_string, os_string
