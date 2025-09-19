d56375c751d2698d5405d4b3826afc8bb818abca
Melissa Autumn <melissa@thunderbird.net>
2025-09-19 10:44:03 -0700

    Fix bodged PR

--- src/thunderbird_accounts/authentication/models.py
+++ src/thunderbird_accounts/authentication/models.py
@@ -23,6 +23,18 @@ class User(AbstractUser, BaseModel):
         ENGLISH = 'en', _('English')
         GERMAN = 'de', _('German')
 
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer."
+        ),
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+
     oidc_id = models.CharField(max_length=256, null=True, help_text=_("OIDC's UID field"))
     last_used_email = models.CharField(max_length=256, null=True, help_text=_('The email previously used to login'))
     language = models.CharField(
