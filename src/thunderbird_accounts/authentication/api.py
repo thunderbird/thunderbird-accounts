322299bab8f1ad3990bd41f4a8497c81fd5793e8
Davi Nakano <davinakanoca@gmail.com>
2025-11-04 13:19:24 -0600

    Delete keycloak session and Django session if user is self logging out

--- src/thunderbird_accounts/authentication/api.py
+++ src/thunderbird_accounts/authentication/api.py
@@ -1,9 +1,12 @@
+import logging
+
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.exceptions import NotAuthenticated, ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from thunderbird_accounts.authentication.middleware import AccountsOIDCBackend
 from thunderbird_accounts.authentication.serializers import UserProfileSerializer
 from thunderbird_accounts.authentication.clients import KeycloakClient
 
@@ -25,19 +28,10 @@ def get_active_sessions(request: Request):
         keycloak_client = KeycloakClient()
         sessions = keycloak_client.get_active_sessions(request.user.oidc_id)
 
-        # Print all keys from request.session
-        print(request.session.keys())
-        print(request.session.items())
-        print(request.session.values())
-        print(request.session.get('session_id'))
-        print(request.session.get('session_key'))
-        print(request.session.get('session_data'))
-        print(request.session.get('session_expiry'))
-        print(request.session.get('session_created'))
-        print(request.session.get('session_updated'))
         return Response(sessions)
     except Exception as e:
-        raise ValidationError(f'Error fetching active sessions: {e}')
+        logging.exception(f'Error fetching active sessions: {e}')
+        raise ValidationError('Error fetching active sessions')
 
 
 @api_view(['POST'])
@@ -51,8 +45,30 @@ def sign_out_session(request: Request):
     if not session_id:
         raise ValidationError('session_id is required')
 
+    oidc_id_token = request.session.get('oidc_id_token')
+
+    if not oidc_id_token:
+        raise ValidationError('No oidc_id_token found in session')
+
     try:
+        # Sign out from the Keycloak session
         keycloak_client = KeycloakClient()
-        return Response(keycloak_client.sign_out_session(session_id))
+        keycloak_client.sign_out_session(session_id)
+
+        # Verify if the request's keycloak session_id matches the one in the ID token
+        auth_backend = AccountsOIDCBackend()
+        payload = auth_backend.verify_token(oidc_id_token)
+        keycloak_session_id = payload.get('sid')
+
+        if not keycloak_session_id:
+            raise ValidationError("'sid' claim not found in ID token. Did you enable back-channel logout in Keycloak?")
+
+        if keycloak_session_id == session_id:
+            # If so, delete current session data and cookie from Django as well
+            request.session.flush()
+
+        return Response({'success': True})
+
     except Exception as e:
-        raise ValidationError(f'Error signing out session: {e}')
+        logging.exception(f'Error signing out session: {e}')
+        raise ValidationError('Error signing out session')
