79db99fe8862d6085320d32361dfeed633da0b37
Melissa Autumn <melissa@thunderbird.net>
2025-11-10 09:28:08 -0800

    Re-work health check to only 200

--- src/thunderbird_accounts/infra/views.py
+++ src/thunderbird_accounts/infra/views.py
@@ -1,15 +1,22 @@
-import datetime
-
 import requests.exceptions
 from django.core.cache import cache
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, JsonResponse
 
 from thunderbird_accounts.authentication.clients import KeycloakClient
+from thunderbird_accounts.authentication.models import User
 from thunderbird_accounts.mail.clients import MailClient
 
 
 def health_check(request: HttpRequest):
-    """Do nothing, only prove the service is responsive."""
+    """Prove the service is responsive."""
+
+    connection_stats = {
+        'keycloak': True,
+        'stalwart': True,
+        'redis': True,
+        'database': True,
+        'accounts': True,
+    }
 
     # Retrieve the list of required actions as a no-op action to demonstrate that keycloak's api is reachable and
     # operational.
@@ -17,23 +24,24 @@ def health_check(request: HttpRequest):
         keycloak_client = KeycloakClient()
         keycloak_client.request('authentication/required-actions')
     except requests.exceptions.HTTPError:
-        return HttpResponse('Keycloak cannot be reached', status=500)
+        connection_stats['keycloak'] = False
 
     # Retrieve telemetry results as a no-op action to demonstrate that stalwart's api is reachable and operational.
     try:
         stalwart_client = MailClient()
         stalwart_client.get_telemetry()
     except requests.exceptions.HTTPError:
-        return HttpResponse('Stalwart cannot be reached', status=500)
+        connection_stats['stalwart'] = False
 
     # Set and retrieve the current timestamp to demonstrate that redis is reachable and operational.
     try:
-        now = datetime.datetime.now(datetime.UTC)
-        cache.set('__tbaccounts_health', now)
-        cache_now = cache.get('__tbaccounts_health')
-        if now != cache_now:
-            return HttpResponse('Redis not returning correct results')
+        cache.get('__tbaccounts_health')
     except Exception:
-        return HttpResponse('Redis cannot be reached', status=500)
+        connection_stats['redis'] = False
 
-    return HttpResponse('Aliveness demonstrated!')
+    try:
+        User.objects.first()
+    except Exception:
+        connection_stats['database'] = False
+
+    return JsonResponse(connection_stats)
