ad9c3494f688a70c9d5442b8c0418201ac4b32f8
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2025-11-10 09:32:46 -0800

    Add keycloak, stalwart and redis checks to health check (Fixes #361) (#371)
    * Add keycloak, stalwart and redis checks to health check (Fixes #361)
    
    # Conflicts:
    #	src/thunderbird_accounts/mail/clients.py
    
    * Re-work health check to only 200

--- src/thunderbird_accounts/infra/views.py
+++ src/thunderbird_accounts/infra/views.py
@@ -1,7 +1,47 @@
-from django.http import HttpRequest, HttpResponse
+import requests.exceptions
+from django.core.cache import cache
+from django.http import HttpRequest, JsonResponse
+
+from thunderbird_accounts.authentication.clients import KeycloakClient
+from thunderbird_accounts.authentication.models import User
+from thunderbird_accounts.mail.clients import MailClient
 
 
 def health_check(request: HttpRequest):
-    """Do nothing, only prove the service is responsive."""
+    """Prove the service is responsive."""
 
-    return HttpResponse('Aliveness demonstrated!')
+    connection_stats = {
+        'keycloak': True,
+        'stalwart': True,
+        'redis': True,
+        'database': True,
+        'accounts': True,
+    }
+
+    # Retrieve the list of required actions as a no-op action to demonstrate that keycloak's api is reachable and
+    # operational.
+    try:
+        keycloak_client = KeycloakClient()
+        keycloak_client.request('authentication/required-actions')
+    except requests.exceptions.HTTPError:
+        connection_stats['keycloak'] = False
+
+    # Retrieve telemetry results as a no-op action to demonstrate that stalwart's api is reachable and operational.
+    try:
+        stalwart_client = MailClient()
+        stalwart_client.get_telemetry()
+    except requests.exceptions.HTTPError:
+        connection_stats['stalwart'] = False
+
+    # Set and retrieve the current timestamp to demonstrate that redis is reachable and operational.
+    try:
+        cache.get('__tbaccounts_health')
+    except Exception:
+        connection_stats['redis'] = False
+
+    try:
+        User.objects.first()
+    except Exception:
+        connection_stats['database'] = False
+
+    return JsonResponse(connection_stats)
