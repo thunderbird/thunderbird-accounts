19c6cc66421530c8985d760465d71244ae2481f4
Davi Nakano <davinakanoca@gmail.com>
2025-11-13 10:43:07 -0600

    Remove and update E2E tests and misc files related to self-serve

--- test/e2e/const/constants.ts
+++ test/e2e/const/constants.ts
@@ -2,12 +2,6 @@
 export const ACCTS_TARGET_ENV = String(process.env.ACCTS_TARGET_ENV);
 
 // tb accounts urls
-export const ACCTS_SELF_SERVE_URL = String(process.env.ACCTS_SELF_SERVE_URL);
-export const ACCTS_SELF_SERVE_CONNECTION_INFO_URL = `${ACCTS_SELF_SERVE_URL}connection-info`;
-export const ACCTS_SELF_SERVE_ACCT_INFO_URL = `${ACCTS_SELF_SERVE_URL}account-settings`;
-export const ACCTS_EMAIL_SIGN_UP_URL = String(process.env.ACCTS_EMAIL_SIGN_UP_URL);
-export const ACCTS_CHECKOUT_URL = `${ACCTS_SELF_SERVE_URL}subscription`;
-export const ACCTS_CHECKOUT_SUCCESS_URL = `${ACCTS_CHECKOUT_URL}/success`;
 export const ACCTS_CONTACT_URL = String(process.env.ACCTS_CONTACT_URL);
 
 // sign-in credentials and corresponding account display name
@@ -35,20 +29,6 @@ export const APP_PASSWORD = 'Your App Password';
 export const YOUR_EMAIL_LBL = 'Your email address:';
 export const THUNDERMAIL_USERNAME = String(process.env.THUNDERMAIL_USERNAME);
 export const THUNDERMAIL_EMAIL_ADDRESS = String(process.env.THUNDERMAIL_EMAIL_ADDRESS);
-
-// email sign-up
-export const EMAIL_SIGN_UP_EMAIL_ADDRESS = 'my-new-email-address';
-export const EMAIL_SIGN_UP_DOMAIN = String(process.env.EMAIL_SIGN_UP_DOMAIN);
-export const EMAIL_SIGN_UP_APP_PWORD = 'password';
-
-// checkout details
-export const CHECKOUT_EMAIL_ADDRESS = String(process.env.EMAIL_SIGN_UP_ADDRESS);
-export const CHECKOUT_COUNTRY = 'US';
-export const CHECKOUT_POSTAL_CODE = '90210';
-export const CHECKOUT_CC_NUM = '4242 4242 4242 4242';
-export const CHECKOUT_CC_NAME = 'first last';
-export const CHECKOUT_CC_EXP = '03/45';
-export const CHECKOUT_CC_CVV = '987';
 
 // mock responses
 export const MOCK_RESPONSE_OK = {
