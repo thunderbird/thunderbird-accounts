a60e66103aa9e85129612283ae3279d26ab2af2e
Davi Nakano <davinakanoca@gmail.com>
2025-11-13 14:33:31 -0600

    Remove vueForms entirely

--- assets/app/app.ts
+++ assets/app/app.ts
@@ -1,37 +1,12 @@
 import { createApp } from 'vue';
 import '@thunderbirdops/services-ui/style.css';
 
-// Forms (Kept as is for now)
-import ContactSupportForm from '@/components/forms/ContactSupportForm.vue';
-
-// Vue App (Everything else)
 import App from '@/App.vue';
+import i18n from '@/composables/i18n';
+import router from '@/router';
 
-const vueForms = [
-  {
-    id: 'contactSupportForm',
-    sfc: ContactSupportForm,
-  },
-];
+const app = createApp(App);
 
-if (window.document.getElementById('app')) {
-  (async () => {
-
-    // Only import router when we're actually on a Vue app route
-    const { default: router } = await import('@/router');
-    const { default: i18n } = await import('@/composables/i18n');
-
-    const app = createApp(App);
-
-    app.use(i18n);
-    app.use(router);
-    app.mount('#app');
-  })();
-} else {
-  for (const vueForm of vueForms) {
-    if (window.document.getElementById(vueForm.id)) {
-      const form = createApp(vueForm.sfc);
-      form.mount(`#${vueForm.id}`);
-    }
-  }
-}
+app.use(i18n);
+app.use(router);
+app.mount('#app');
