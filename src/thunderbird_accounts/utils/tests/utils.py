561ab82a1ae2be0538ee3256085c3e0641de9722
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2025-11-24 15:21:16 -0800

    Fx 500 on checkout complete (#454)
    * Fix bug in get_user with mozilla oidc library
    
    * Fix bug in on_paddle_checkout_complete and add view tests

--- src/thunderbird_accounts/utils/tests/utils.py
+++ src/thunderbird_accounts/utils/tests/utils.py
@@ -1,6 +1,22 @@
 from typing import Optional
+from django.test import Client as RequestClient
 
 from requests import Response
+
+from thunderbird_accounts.authentication.models import User
+
+
+def oidc_force_login(client: RequestClient, user: User, backend=None):
+    """This works like :any:`django.test.Client` except it also bypasses the missing
+    ``oidc_id_token_expiration`` session value by setting it in the far future.
+
+    Other-wise the request would be redirected to keycloak to login."""
+    client.force_login(user, backend=backend)
+
+    # Mock OIDC session data to prevent SessionRefresh middleware from redirecting
+    session = client.session
+    session['oidc_id_token_expiration'] = 9999999999  # Far future timestamp
+    session.save()
 
 
 def build_keycloak_success_response(return_json: Optional[dict] = None, return_headers: Optional[dict] = None):
@@ -31,6 +47,7 @@ def build_mail_get_account(return_json: Optional[dict] = None, return_headers: O
     fake_response.headers = return_headers or {}
     fake_response.json = lambda: return_json or default_return
     return fake_response
+
 
 def build_mail_update_account(return_json: Optional[dict] = None, return_headers: Optional[dict] = None):
     """Builds a successful stalwart update account request response."""
