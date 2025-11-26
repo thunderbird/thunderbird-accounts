5c136316a87707ccc2c794f1145f38a7ec99955d
Melissa Autumn <melissa@thunderbird.net>
2025-11-26 11:39:43 -0800

    Lint fix

--- src/thunderbird_accounts/authentication/admin/models.py
+++ src/thunderbird_accounts/authentication/admin/models.py
@@ -11,7 +11,7 @@ from thunderbird_accounts.authentication.admin.actions import (
 from thunderbird_accounts.authentication.admin.forms import CustomUserChangeForm, CustomNewUserForm
 from thunderbird_accounts.authentication.clients import KeycloakClient
 from thunderbird_accounts.authentication.exceptions import DeleteUserError
-from thunderbird_accounts.authentication.models import User, AllowListEntry
+from thunderbird_accounts.authentication.models import User
 from thunderbird_accounts.mail.clients import MailClient
 
 
