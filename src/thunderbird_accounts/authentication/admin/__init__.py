def99872c1a3a317e7cce60bfb04f69bb6a74d28
Melissa Autumn <melissa@thunderbird.net>
2026-01-27 10:32:31 -0800

    Fix import issue

--- src/thunderbird_accounts/authentication/admin/__init__.py
+++ src/thunderbird_accounts/authentication/admin/__init__.py
@@ -1,7 +1,7 @@
 """Admin module. Anything with the user app in the admin panel is defined here."""
 
 from django.contrib import admin
-from django.contrib.admin import models
+from django.contrib.admin.models import LogEntry
 
 from thunderbird_accounts.authentication.admin.models import CustomUserAdmin, AllowListEntryAdmin, LogEntryAdmin
 from thunderbird_accounts.authentication.models import User, AllowListEntry
@@ -9,4 +9,4 @@ from thunderbird_accounts.authentication.models import User, AllowListEntry
 # Register the User admin here
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(AllowListEntry, AllowListEntryAdmin)
-admin.site.register(models.LogEntry, LogEntryAdmin)
+admin.site.register(LogEntry, LogEntryAdmin)
