462dc8194290c35281484236df5d0cf8bd505a60
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2026-01-27 11:49:49 -0800

    Show all log entries for super users. (#546)
    * Show all log entries for super users.
    
    * Lint fix
    
    * Fix import issue

--- src/thunderbird_accounts/authentication/admin/__init__.py
+++ src/thunderbird_accounts/authentication/admin/__init__.py
@@ -1,10 +1,12 @@
 """Admin module. Anything with the user app in the admin panel is defined here."""
 
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 
-from thunderbird_accounts.authentication.admin.models import CustomUserAdmin, AllowListEntryAdmin
+from thunderbird_accounts.authentication.admin.models import CustomUserAdmin, AllowListEntryAdmin, LogEntryAdmin
 from thunderbird_accounts.authentication.models import User, AllowListEntry
 
 # Register the User admin here
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(AllowListEntry, AllowListEntryAdmin)
+admin.site.register(LogEntry, LogEntryAdmin)
