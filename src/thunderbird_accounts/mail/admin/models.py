290c90df402acbdbdb4174153745138b7946e3b3
Melissa Autumn <melissa@thunderbird.net>
2026-04-28 11:10:15 -0700

    Add filtering and search to the account/domain admin panel

--- src/thunderbird_accounts/mail/admin/models.py
+++ src/thunderbird_accounts/mail/admin/models.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from thunderbird_accounts.mail.admin.actions import admin_fix_stalwart_ids
 from thunderbird_accounts.mail.admin.forms import CustomEmailBaseForm, CustomAccountBaseForm
@@ -21,9 +22,31 @@ class AccountAdmin(admin.ModelAdmin):
     inlines = (EmailInline,)
     readonly_fields = ('uuid', 'stalwart_id', 'stalwart_created_at', 'stalwart_updated_at')
 
+    search_fields = ('name',)
+    search_help_text = _('Search accounts by name.')
+    list_filter = ['created_at', 'updated_at']
+    list_display = (
+        'name',
+        'quota',
+        'user',
+        'created_at',
+        'updated_at',
+    )
+
 
 class DomainAdmin(admin.ModelAdmin):
     actions = [admin_fix_stalwart_ids]
 
     model = Domain
     readonly_fields = ('uuid', 'stalwart_id', 'stalwart_created_at', 'stalwart_updated_at')
+
+    search_fields = ('name',)
+    search_help_text = _('Search domains by name.')
+    list_filter = ['created_at', 'updated_at']
+    list_display = (
+        'name',
+        'status',
+        'user',
+        'created_at',
+        'updated_at',
+    )
