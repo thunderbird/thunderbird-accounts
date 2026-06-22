549cb1646b86bcdad902b8311da0c629857c4602
Mel <97147377+MelissaAutumn@users.noreply.github.com>
2026-06-22 10:13:45 -0700

    Don't display an html response for drj api endpoints on non-debug envs (Fixes #993) (#994)

--- src/thunderbird_accounts/settings.py
+++ src/thunderbird_accounts/settings.py
@@ -287,6 +287,11 @@ REST_FRAMEWORK = {
     },
 }
 
+if not DEBUG:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 REDIS_URL = os.getenv('REDIS_URL')
 AVAILABLE_CACHES = {
     'dev': {
