018678b8a623a63b9d741ee3a893829887e33e93
Rob Wood <rwood@thunderbird.net>
2026-07-20 17:58:02 -0400

    Fix sign-in race condition on android (#1130)

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   // Global timeout: Playwright will timeout if the entire session (includes all test runs) exceeds this.
   // Must take into account running on mulitple browsers (and BrowserStack is much slower too!). Odds are the
   // tests will time out at the locator/test level first anyway; but there is no default so best to specify
-  globalTimeout: 10 * 60 * 1000,
+  globalTimeout: 12 * 60 * 1000,
   // Individual test timeout - a single test will time out if it is still running after this time (ms)
   timeout: 150 * 1000, // 2.5 minutes
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
