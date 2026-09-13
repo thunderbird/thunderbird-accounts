2f4db3f9ff229279b7081417bddb0bf8a88f0b34
Mark Stosberg <mark@stosberg.com>
2026-09-13 10:31:13 -0400

    fixup! Settings page doesn't have welcome header

--- assets/app/vue/views/MailView/views/SecuritySettingsView/index.vue
+++ assets/app/vue/views/MailView/views/SecuritySettingsView/index.vue
@@ -8,7 +8,6 @@ import CardContainer from '@/components/CardContainer.vue';
 import AccountActivity from './components/AccountActivity.vue';
 import ConnectedApps from './components/ConnectedApps.vue';
 import AllowList from './components/AllowList.vue';
-import WelcomeHeader from '../../sections/DashboardSection/components/WelcomeHeader.vue';
 
 const { t } = useI18n();
 </script>
@@ -21,8 +20,6 @@ export default {
 
 <template>
   <div class="security-settings-view">
-    <welcome-header />
-
     <card-container
       class="security-settings-card"
       :title="t('views.mail.views.securitySettings.securitySettings')"
