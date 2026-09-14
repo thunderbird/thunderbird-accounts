3bbad869d86390fffb312d39fb151d6f13849f11
Mark Stosberg <mark@stosberg.com>
2026-09-14 10:45:24 -0400

    fixup! Remove Active Sessions / Connected Apps from "Dashboard". It's now on "Settings.

--- assets/app/vue/views/DashboardView/components/PrivacyAndDataCard.vue
+++ assets/app/vue/views/DashboardView/components/PrivacyAndDataCard.vue
@@ -1,63 +1,24 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
-import { PrimaryButton, VisualDivider } from '@thunderbirdops/services-ui';
+import { PrimaryButton } from '@thunderbirdops/services-ui';
 import CardContainer from '@/components/CardContainer.vue';
-import { signOutAllSessions } from '@/views/MailView/views/SecuritySettingsView/api';
 
 const { t } = useI18n();
-
-const logoutFromAllSessions = () => signOutAllSessions();
 </script>
 
 <template>
   <card-container class="privacy-and-data-card">
     <h2>{{ t('views.dashboard.privacyAndDataCard.privacyAndData') }}</h2>
 
-    <div class="privacy-and-data-card-details">
-      <div>
-        <strong>{{ t('views.dashboard.privacyAndDataCard.activeSessions') }}</strong>
-        <p>{{ t('views.dashboard.privacyAndDataCard.activeSessionsText') }}</p>
-    
-        <div class="active-sessions-actions">
-          <router-link v-slot="{ href, navigate }" custom to="/settings#current-sign-ins">
-            <primary-button :href="href" variant="outline" size="small" class="manage-active-sessions-button" @click="navigate">
-              {{ t('views.dashboard.privacyAndDataCard.manageActiveSessionsButtonLabel') }}
-            </primary-button>
-          </router-link>
+    <div>
+      <strong>{{ t('views.dashboard.privacyAndDataCard.deleteAccount') }}</strong>
+      <p>{{ t('views.dashboard.privacyAndDataCard.deleteAccountText') }}</p>
 
-          <div class="link-text">
-            <a href="/logout/" @click.prevent="logoutFromAllSessions">
-              {{ t('views.dashboard.privacyAndDataCard.logoutFromAllSessionsLink') }}
-            </a>
-          </div>
-        </div>
-      </div>
-  
-      <visual-divider/>
-
-      <div>
-        <strong>{{ t('views.dashboard.privacyAndDataCard.connectedApps') }}</strong>
-        <p>{{ t('views.dashboard.privacyAndDataCard.connectedAppsText') }}</p>
-
-        <router-link v-slot="{ href, navigate }" custom to="/settings#connected-apps">
-          <primary-button :href="href" variant="outline" size="small" class="manage-connected-apps-button" @click="navigate">
-            {{ t('views.dashboard.privacyAndDataCard.manageConnectedAppsButtonLabel') }}
-          </primary-button>
-        </router-link>
-      </div>
-
-      <visual-divider type="horizontal" />
-  
-      <div>
-        <strong>{{ t('views.dashboard.privacyAndDataCard.deleteAccount') }}</strong>
-        <p>{{ t('views.dashboard.privacyAndDataCard.deleteAccountText') }}</p>
-
-        <router-link v-slot="{ href, navigate }" custom to="/contact">
-          <primary-button :href="href" variant="outline" size="small" class="delete-account-button" @click="navigate">
-            {{ t('views.dashboard.privacyAndDataCard.deleteAccountButtonLabel') }}
-          </primary-button>
-        </router-link>
-      </div>
+      <router-link v-slot="{ href, navigate }" custom to="/contact">
+        <primary-button :href="href" variant="outline" size="small" class="delete-account-button" @click="navigate">
+          {{ t('views.dashboard.privacyAndDataCard.deleteAccountButtonLabel') }}
+        </primary-button>
+      </router-link>
     </div>
   </card-container>
 </template>
@@ -66,12 +27,6 @@ const logoutFromAllSessions = () => signOutAllSessions();
 .privacy-and-data-card {
   min-width: unset;
   color: var(--colour-ti-secondary);
-
-  .privacy-and-data-card-details {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-  }
 
   h2 {
     font-family: metropolis;
@@ -92,52 +47,6 @@ const logoutFromAllSessions = () => signOutAllSessions();
   p {
     margin-block-end: 1rem;
     line-height: 1.32;
-  }
-
-  .active-sessions-actions {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 1rem;
-  }
-
-  .link-text {
-    display: flex;
-    align-items: center;
-    min-height: 2.75rem;
-
-    a {
-      width: auto;
-      height: 0.938rem;
-      flex-grow: 0;
-      font-family: Inter;
-      font-size: 0.75rem;
-      font-weight: normal;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: normal;
-      letter-spacing: normal;
-      text-align: left;
-      color: var(--text-icon-highlight);
-      white-space: nowrap;
-    }
-  }
-
-  .manage-active-sessions-button,
-  .manage-connected-apps-button {
-    --button-outline-border: linear-gradient(to bottom, #58c9ff 131%, var(--text-icons-highlight-blue) 85%, #175fb6 -8%);
-    width: 12.125rem;
-    height: 2.75rem;
-    flex-grow: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 8px;
-    padding: 1.125rem 1rem;
-    border-radius: 8px;
-    background-color: var(--neutral-0);
-    white-space: nowrap;
   }
 
   .delete-account-button {
