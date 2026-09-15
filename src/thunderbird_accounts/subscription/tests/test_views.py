c18c49a4ea3a8ddef60491aec5c1c3b126fc572b
MooseTheRebel <MooseTheRebel@users.noreply.github.com>
2026-09-15 14:00:52 -0400

    fix: test_subscription_plan_info_account_not_found

--- src/thunderbird_accounts/subscription/tests/test_views.py
+++ src/thunderbird_accounts/subscription/tests/test_views.py
@@ -309,6 +309,9 @@ class ActiveSubscriptionRequiredViewTestCase(TestCase):
                 response = self.client.post(reverse('subscription_plan_info'), HTTP_ACCEPT='application/json')
 
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.json(), {'success': False, 'error': 'User not found'})
+        self.assertEqual(
+            response.json(),
+            {'success': False, 'error': 'Subscription plan information is not available at this moment.'},
+        )
         self.assertTrue(any(record.levelname == 'WARNING' for record in logs.records))
         self.assertFalse(any(record.levelname == 'ERROR' for record in logs.records))
