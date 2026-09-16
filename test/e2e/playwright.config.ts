51ccb15061523b2627ac43cd6fa7234ce7402a29
rwood-moz <rwood@thunderbird.net>
2026-09-16 16:07:47 -0400

    Add E2E test summary report

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -43,8 +43,12 @@ export default defineConfig({
   globalTimeout: 12 * 60 * 1000,
   // Individual test timeout - a single test will time out if it is still running after this time (ms)
   timeout: 150 * 1000, // 2.5 minutes
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html']],
+  /* Generate HTML in local development and local-stack CI only. */
+  reporter: process.env.CI
+    ? process.env.GENERATE_PLAYWRIGHT_HTML_REPORT === 'true'
+      ? [['dot'], ['./reporters/github-summary-reporter.ts'], ['html']]
+      : [['dot'], ['./reporters/github-summary-reporter.ts']]
+    : [['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
